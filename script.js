41aed7fce8419e1e7bd7bc1ddf160432f870ade4
guillaume <guillaume.reboullet@epitech.eu>
2024-02-19 22:48:47 +0100

    Push premiere version de la homepage, avec 10 faux tweet, ajout de js pour la nav cacher, ajout de css pour faire un site propre, et premiere version de la structure html pour la homepage

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    let button_pp = document.getElementById('button_pp')
+    let nav_section = document.getElementById('nav_section')
+    let hide = true
+
+    button_pp.addEventListener('click', ()=>{
+        if(hide){
+            nav_section.style.display="block"
+            hide = false
+        }else{
+            nav_section.style.display="none"
+            hide = true
+        }
+    })
+
+    let header = document.getElementsByTagName('header')
+}
