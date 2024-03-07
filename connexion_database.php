686718dae908a15f91e4e6d47ba80f9c80596696
wac <rodrigo.lisboa@epitech.eu>
2024-03-07 17:29:18 +0100

    User Account Not Functionnal

--- connexion_database.php
+++ connexion_database.php
@@ -1,10 +1,4 @@
 <?php
-session_start();
-
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: connexion.php");
-    exit;
-    }
 
 class Database {
 
@@ -14,7 +8,7 @@ class Database {
     private $db_name;
     private $pdo;
 
-    public function __construct($db_name = "twitter", $db_host = "localhost", $db_user = "zoewac", $db_pass ="dykehqb9") {
+    public function __construct($db_name, $db_host = "localhost", $db_user = "zoe", $db_pass ="dykehqb9") {
         $this->db_host = $db_host;
         $this->db_name = $db_name;
         $this->db_user = $db_user;
@@ -28,23 +22,25 @@ class Database {
                 $this->pdo = new PDO($dsn, $this->db_user, $this->db_pass);
                 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
-                throw new InvalidArgumentException("Database error:" . $e->getMessage());
+                echo "Database error: " . $e->getMessage();
+                throw $e;
             }
         }
         return $this->pdo;
     }
-    
-
-    public function getUserData() {
-        try{
-            $username = $_POST['username'];
-            $sql = "SELECT username, at_user_name, profile_picture, bio, banner, birthdate, private, creation_time, city FROM user WHERE at_user_name LIKE '$username'";
-            $stmt = $this->getPDO()->prepare($sql);
-            $stmt->execute();
-        }catch(PDOException $e) {
+}
+//POUR FAIRE UNE REQUETE SQL
+try {
+            $sql = "INSERT INTO tweet (id_user, content) VALUES (:id_user, :content)";
+            $statement = $this->getPDO()->prepare($sql);
+            $statement->bindParam(':id_user', $userId);
+            $statement->bindParam(':content', $content);
+            $statement->execute();
+        } catch(PDOException $e) {
             echo "Database error: " . $e->getMessage();
             throw $e;
         }
-    }
-}
+throw new InvalidArgumentException;
+throw new InvalidArgumentException("Database error: " . $e->getMessage());
+    
 ?>
